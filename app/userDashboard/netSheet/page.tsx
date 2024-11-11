d0a791827b2fdbc049b9fe3ef1b27f26e3f2121f
BigT001 <unclet1992@gmail.com>
2024-11-11 12:13:04 -0800

    update Netsheet/spresdsheet

--- app/userDashboard/netSheet/page.tsx
+++ app/userDashboard/netSheet/page.tsx
@@ -3,9 +3,9 @@ import { Spreadsheet } from '@/components/ui/spreadsheet';
 export default function NetSheet() {
   return (
     <div className="p-4">
-      <Spreadsheet 
-        rows={15} 
-        columns={10} 
+      <Spreadsheet
+        initialRows={15}
+        initialColumns={10}
         className="bg-white rounded-lg shadow-lg"
       />
     </div>
